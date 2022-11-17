35536a45fd19b24118d4fac9cea284ae4e3df655
Dmitry <extern.zamyatin_dmitry@allianz.de>
2022-11-17 21:14:08 +0100

    daily

--- lua/dz.sql
+++ lua/dz.sql
@@ -1,6 +1,11 @@
 --/
 create or replace script gesamttest.dz returns rowcount as
 
+/*
+  import('gesamttest.dz', 'dz')
+  local newline, dquery, Set, flat_array, map_null = dz.newline, dz.dquery, dz.Set, dz.flat_array, dz.map_null
+*/
+
   newline = string.char(13) .. string.char(10)
   
   function Set (list)
@@ -11,11 +16,22 @@ create or replace script gesamttest.dz returns rowcount as
 
   function dquery(stmt, p_params)
     -- https://docs.exasol.com/db/latest/database_concepts/scripting/db_interaction.htm
+    -- todo: _assert_rows = 0/1/...
+    -- todo: flatten res = 
+    -- todo: suc weg, keep only res
     local params = p_params or {}
     local suc, res = pquery(stmt, params)
     output("dquery: " .. res.statement_text)
     assert(suc, "NOK: " .. tostring(res.error_message))
-    if res.rows_inserted or res.rows_updated or res.rows_deleted then output("inserted: " .. res.rows_inserted .. ", updated: "  .. res.rows_updated .. ", deleted: "  .. res.rows_deleted) end
+    if res.rows_affected 
+      then output("rows inserted: " .. res.rows_inserted .. ", updated: "  .. res.rows_updated .. ", deleted: "  .. res.rows_deleted) 
+      else 
+        local cols = #res==0 and '' or ", cols: " .. #res[1]
+        output("rows: " .. #res .. cols)
+    end
+    
+    -- todo: if execute script then output(table.concat(flat_array(res), newline))
+    
     return suc, res
   end  
 
@@ -26,6 +42,18 @@ create or replace script gesamttest.dz returns rowcount as
       new_arr[i] = arr[i][ind] 
     end
     return new_arr
+  end
+
+  function map_null(arr, v_null)
+    for i=1, #arr do
+      if arr[i] == null then
+        if v_null ~=  null
+          then arr[i] = v_null
+          else table.remove(arr, i)
+        end
+      end
+    end
+    return arr
   end
   
 ;
--- lua/template.sql
+++ lua/template.sql
@@ -1,70 +1,51 @@
 --/
-create or replace script gesamttest.sql_create_table_staging(p_table_name, p_action) returns rowcount as
+create or replace script gesamttest.sql_create_table(p_table_name, p_action) returns rowcount as
   output("execute script " .. string.lower(exa.meta.script_schema .. "." .. exa.meta.script_name) .. "('" .. p_table_name .. "', '" .. p_action .. "') with output;")
 
-  local newline = string.char(13)..string.char(10)
+  import('gesamttest.dz', 'dz')
+  local newline, dquery, Set, flat_array, map_null = dz.newline, dz.dquery, dz.Set, dz.flat_array, dz.map_null
   
-  function Set (list)
-    local set = {}
-    for _, l in ipairs(list) do set[l] = true end
-    return set
-  end
-
-  function dquery(stmt, params)
-    -- https://docs.exasol.com/db/latest/database_concepts/scripting/db_interaction.htm
-    output(type(params))
-    output(#params)
-    output(params.t)
-    params = params or {}
-    for i,j in pairs(params) do output(i..' '..j) end
-    local suc, res = pquery(stmt, params)
-    output("dquery: " .. res.statement_text)
-    assert(suc, "NOK: " .. tostring(res.error_message))
-    if res.rows_inserted or res.rows_updated or res.rows_deleted then output("inserted: " .. res.rows_inserted .. ", updated: "  .. res.rows_updated .. ", deleted: "  .. res.rows_deleted) end
-    return suc, res
-  end  
-
-  function flat_array(arr, ind)
-    local ind = ind or 1
-    local new_arr = {}
-    for i=1, #arr do
-      new_arr[i] = arr[i][ind] 
-    end
-    return new_arr
-  end
-  
-  table_name = p_table_name
+  local table_name = p_table_name
   
   local actions = Set {'show', 'execute'}
   if not actions[p_action] then error('Unknown action = ' .. tostring(p_action)) end
   local action = p_action
 
-  q = [[
-    select * from gesamttest.conf_columns tc
-    join gesamttest.conf_staging_v ts on ts.to_table = tc.col_table and ts.to_schema = tc.col_schema
-    where ts.table_name = :t
-    order by col_position
-  ]]
-  
-  local suc, res = dquery(q, {t=table_name})
+
+  local suc, res = dquery([[select * from gesamttest.conf_columns_v where table_name = :t order by col_position]], {t=table_name})
   assert(#res > 0, 'NOK #res = ' .. #res)
+  
   local sql_out_arr = {}
-  table.insert(sql_out_arr, 'create or replace table ' .. res[1].TO_SCHEMATABLE .. ' (')
-  for row = 1, #res do
+  table.insert(sql_out_arr, 'create or replace table ' .. res[1].COL_SCHEMATABLE .. ' (')
+  for i = 1, #res do
+    local col = res[i]
+    local col_name = col.COL_NAME
+    if col.COL_SCHEMA == 'DWH_TECH' then col_name = col.TABLE_PREFIX .. '_' .. col_name end
+    
     local default = ""
+    if col.COL_DEFAULT ~= NULL      then default = default .. " default " .. col.COL_DEFAULT end 
+    
     local comment = ""
-    if res[row].COL_DEFAULT ~= NULL      then default = default .. " DEFAULT " .. res[row].COL_DEFAULT end 
-    if res[row].COL_COMMENT_SQL ~= NULL  then comment = comment .. " COMMENT IS '" .. res[row].COL_COMMENT_SQL .. "'" end
-    comment = comment .. (row < #res and "," or "")
-    if res[row].COL_COMMENT_CODE ~= NULL then comment = comment .. " /* " .. res[row].COL_COMMENT_CODE .. " */" end
-    table.insert(sql_out_arr, '  ' .. res[row].COL_NAME .. ' ' .. res[row].COL_TYPE .. default .. comment)
+    if col.COL_COMMENT_SQL ~= NULL  then comment = comment .. " comment is '" .. col.COL_COMMENT_SQL .. "'" end
+    comment = comment .. (i < #res and "," or "")
+    if col.COL_COMMENT_CODE ~= NULL then comment = comment .. " /* " .. col.COL_COMMENT_CODE .. " */" end
+    table.insert(sql_out_arr, '  ' .. col_name .. ' ' .. col.COL_TYPE .. default .. comment)
   end      
   table.insert(sql_out_arr, ');')
-  sql_out = table.concat(sql_out_arr, newline)
-  output(sql_out)
+  local sql_out = table.concat(sql_out_arr, newline)
+
+  if action == 'show' then
+    output(sql_out)
+
+  elseif action == 'execute' then
+    local suc, res = dquery(sql_out)
+    output(table.concat(map_null(flat_array(res),''), newline))
+
+  else error('Unknown action = ' .. tostring(p_action))
+  end
 
 ;
 /
 
-execute script gesamttest.sql_create_table_staging('DIM_VERTRIEBSGEBIET_JES', 'show') with output;
+execute script gesamttest.sql_create_table('SACH_STORNO_MONAT', 'show') with output;
 
